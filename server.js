0280dc30ac9033f593a98fdeb19f37106f3c7594
da kong <kongda9999@gmail.com>
2021-12-07 08:41:01 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+  const { Server } = require('ws');
+
+const wss = new Server({ server });
+
+let streamingSessions = {};
+let receive = ""
+let transmit = ""
+const recordingDirectory = resolve(__dirname, 'recordings');
+
+// Handle Web Socket Connection
+wss.on("connection", function connection(ws) {
+    console.log("New Connection Initiated");
+
+    ws.on("message", function incoming(message) {
+        const msg = JSON.parse(message);
+        console.log(msg);
+        let streamingSession = {};
+        switch (msg.event) {
+            case "CONNECTED":
+                console.log(`A new call has connected.`);
+                break;
+            case "START":
+                console.log('Starting Media Stream');
+                // streamingSession = {
+                //     metadata: {},
+                //     id: ""
+                // };
+                // streamingSession[websocket] = streaming_session
+                // streaming_session["metadata"] = message["metadata"]
+                // streaming_session["id"] = str(streaming_session["metadata"]["callId"]) + "-" + str(streaming_session["metadata"]["sessionId"])
+                break;
+            case "MEDIA":
+                console.log(`Receiving Audio...`)
+                break;
+            case "STOP":
+                console.log(`Call Has Ended`);
+                break;
+        }
+    });
+});
